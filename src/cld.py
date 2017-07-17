6b10ecab518c385fa38f2a767bd59cc54d661b1f
eugene <eugene.nikolchev@curbside.in>
2017-07-16 19:15:05 -0700

    Check auto update

--- src/cld.py
+++ src/cld.py
@@ -6,7 +6,7 @@ sys.path.append(libs)
 from workflow import Workflow
 from bs4 import BeautifulSoup
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 def main(wf):
     url = "http://clojuredocs.org"
